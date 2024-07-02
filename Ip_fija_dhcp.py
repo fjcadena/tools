9746d6cd00b05ef75ffffa414f48ed39831e223f
fjcadena <126709126+fjcadena@users.noreply.github.com>
2024-07-02 19:58:31 +0200

    Update Ip_fija_dhcp.py
    se ha añadido la parte de listado de IP fijas, no solo las redes ahora ya lista las IP en un diccionario

--- Ip_fija_dhcp.py
+++ Ip_fija_dhcp.py
@@ -21,21 +21,46 @@ def es_ip_valida(ip):
 
 def subredes(configuracion):
     redes = []
+    bloque = []
+    zone = False
     for l in configuracion:
         try:
             if l.startswith('subnet '):
                 ip = l.split(' ')[1]
                 if ip:
                     if es_ip_valida(ip):
-                        print(f"{ip} es una ip valida")
                         redes.append(ip)
+            if l.startswith('host '):
+                zone = True
+            if zone:
+                bloque.append(l)
+                if '}' in l:
+                    zone = False
+
         except Exception as e:
+            zone = False
             print(f"Ocurrio un error: {e}")
+    list_ip(bloque)
     print(f"listado de redes\n{redes}")
+    #4print(f" Listado de ips\n{bloque}")
 
-def list_ip():
-    pass
-
+def list_ip(bloque):
+    i = 0
+    dict_host = {}
+    while i < len(bloque):
+        salto = 1
+        if bloque[i].startswith('host '):
+            clave = bloque[i].split(' ')[1]
+            if '{' in bloque[i]: # inicio de bloque
+                while not ((i + salto > len(bloque)) or ('}' in bloque[i + salto]) or (bloque[i + salto].startswith('fixed-address '))):
+                    salto += 1
+                if bloque[i + salto].startswith('fixed-address '):
+                    valor = bloque[i + salto].split(' ')[1]
+                    if valor[-1] == ';':
+                        valor = valor[:-1]
+            dict_host[clave] = valor
+        i += salto
+    print(dict_host)
 ruta_fichero = './doc/dhcpd.conf'
 config = leer_file_dhcp(ruta_fichero)
 subredes(config)
